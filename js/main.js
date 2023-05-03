8348956bead2663f81dcf2862915e7bf9b3b1321
Jeff Drury <me@jeffdrury.com>
2023-05-03 09:27:12 -0400

    Add scroll to top link

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const studentsList = students.map(function(s){
 	return `
 		<div class="card" ${s.href==='#' ? `data-icon=${s.name.toLowerCase()}` : 'data-name' }>
 			<div class="image">
-			  <a href="${s.href}"><img src="img/${s.image}" alt="${s.name} logo"></a>
+			  <a href="${s.href}" rel="external"><img src="img/${s.image}" alt="${s.name} logo"></a>
 			</div>
 			<div class="text">
 			  ${s.href==='#' ? '' : `<h2>${s.name}</h2>`}
@@ -18,5 +18,5 @@ const studentsEl = document.querySelector('.students');
 studentsEl.innerHTML = studentsList.join('');
 
 // // open links in new window
-const links = document.querySelectorAll('a');
+const links = document.querySelectorAll('a[rel="external]');
 links.forEach(l => l.setAttribute('target','_blank'));
